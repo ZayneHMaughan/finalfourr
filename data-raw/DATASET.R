f3aa08850dbebd466855273d9c2dd57588ae4bf4
Zayne <maughanz@byu.edu>
2025-04-10 14:32:18 -0600

    Adjust File

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -68,7 +68,7 @@ season_stats |>
   ) |>
   dplyr::select( !c(G, W, "ADJDE", "BARTHAG", "EFG_O",
              "EFG_D", "TOR", "TORD", "ORB","DRB",
-       "FTR", "FTRD","X2P_O" ,"X2P_D", "X3P_O", "X3P_D", "ADJ_T", "WAB" )
+       "FTR", "FTRD","X2P_O" ,"X2P_D", "X3P_O", "X3P_D", "ADJ_T", "WAB", "ADJOE" )
   ) -> DATASET
 
 
